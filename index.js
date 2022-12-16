38fedf13030f980564c4f5fe7fd25c9a8c6c88d0
Julien Ramboz <ramboz@adobe.com>
2022-12-16 14:19:47 +0100

    test: add unit tests

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
+/* eslint-disable no-console */
+
 const LABEL_COLORS = {
   misc: '#444',
   cls: '#c50',
@@ -32,13 +34,13 @@ const DEFAULT_OPTIONS = {
   lcp: true,
   tbt: true,
   resources: true,
-}
+};
 
-function log(message, time = new Date() - performance.timeOrigin, type = 'misc', colorOverride) {
-  const color = colorOverride || LABEL_COLORS[type] || LABEL_COLORS.misc;
+function log(message, time = new Date() - performance.timeOrigin, type = 'misc', colorOverride = '') {
+  const color = colorOverride || LABEL_COLORS[type];
   console.log(
     `%c${Math.round(time).toString().padStart(5, ' ')}%c %c${type}%c ${message}`,
-    'background-color: #444; padding: 3px; border-radius: 3px;',
+    `background-color: ${LABEL_COLORS.misc}; padding: 3px; border-radius: 3px;`,
     '',
     `background-color: ${color}; padding: 3px 5px; border-radius: 3px;`,
     '',
@@ -49,121 +51,154 @@ function pad(value, length = 5, filler = ' ') {
   return value.toString().padStart(length, filler);
 }
 
+export function domContentLoadedReporter(options) {
+  return (entry) => {
+    if (entry.loadEventStart) {
+      const total = entry.loadEventEnd - entry.loadEventStart;
+      log(`${entry.name}. Load handler took ${total.toFixed(2)}ms`, entry.loadEventStart, 'load', 'darkred');
+    } else {
+      const total = entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart;
+      log(`${entry.name}. DomContentLoaded handler took ${total.toFixed(2)}ms`, entry.domContentLoadedEventStart, 'dcl');
+    }
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
 function trackDomContentLoaded(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      if (entry.loadEventStart) {
-        const total = entry.loadEventEnd - entry.loadEventStart;
-        log(`${entry.name}. Load handler took ${total.toFixed(2)}ms`, entry.loadEventStart, 'load', 'darkred');
-      } else {
-        const total = entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart;
-        log(`${entry.name}. DomContentLoaded handler took ${total.toFixed(2)}ms`, entry.domContentLoadedEventStart, 'dcl');
-      }
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntries().forEach(domContentLoadedReporter(options));
   });
   observer.observe({ type: 'navigation', buffered: true });
 }
 
+export function firstContentfulPaintReporter(options) {
+  return (entry) => {
+    log('', entry.startTime, 'fcp');
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
 function trackFirstContentfulPaint(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntriesByName('first-contentful-paint').forEach((entry) => {
-      log('', entry.startTime, 'fcp');
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntriesByName('first-contentful-paint').forEach(firstContentfulPaintReporter(options));
   });
   observer.observe({ type: 'paint', buffered: true });
+}
+
+export function firstInputDelayReporter(options) {
+  return (entry) => {
+    log(`${entry.name} took ${entry.duration}ms`, entry.startTime, 'fid');
+    console.log(entry.target);
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
 }
 
 function trackFirstInputDelay(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      log(`${entry.name} took ${entry.duration}ms`, entry.startTime, 'fid');
-      console.log(entry.target);
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntries().forEach(firstInputDelayReporter(options));
   });
   observer.observe({ type: 'first-input', buffered: true });
 }
 
+export function largestContentfulPaintReporter(options) {
+  return (entry) => {
+    log(entry.url, entry.startTime, 'lcp');
+    console.log(entry.element);
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
 function trackLagestContentfulPaint(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      log(entry.url, entry.startTime, 'lcp');
-      console.log(entry.element);
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntries().forEach(largestContentfulPaintReporter(options));
   });
   observer.observe({ type: 'largest-contentful-paint', buffered: true });
 }
 
+export function cumulativeLayoutShiftReporter(options) {
+  return (entry) => {
+    log(`${Math.round(entry.value * 100000) / 100000}ms`, entry.startTime, 'cls');
+    (entry.sources || []).forEach((source) => {
+      const to = source.currentRect;
+      const from = source.previousRect;
+      const { node } = source;
+      console.log(
+        `        from: ${pad(from.top, 4)} ${pad(from.right, 4)} ${pad(from.bottom, 4)} ${pad(from.left, 4)}\n`
+        + `        to:   ${pad(to.top, 4)} ${pad(to.right, 4)} ${pad(to.bottom, 4)} ${pad(to.left, 4)}`,
+      );
+      console.log(node.nodeType === Node.TEXT_NODE ? node.parentElement : node);
+    });
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
 function trackCumulativeLayoutShifts(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      log(`${Math.round(entry.value * 100000) / 100000}ms`, entry.startTime, 'cls');
-      entry.sources.forEach((source) => {
-        const to = source.currentRect;
-        const from = source.previousRect;
-        let { node } = source;
-        console.log(
-          `        from: ${pad(from.top, 4)} ${pad(from.right, 4)} ${pad(from.bottom, 4)} ${pad(from.left, 4)}\n` +
-          `        to:   ${pad(to.top, 4)} ${pad(to.right, 4)} ${pad(to.bottom, 4)} ${pad(to.left, 4)}`);
-        console.log(node.nodeType === Node.TEXT_NODE ? node.parentElement : node);
-        if (options.debug) {
-          console.log(JSON.stringify(entry));
-        }
-      });
-    });
+    list.getEntries().forEach(cumulativeLayoutShiftReporter(options));
   });
   observer.observe({ type: 'layout-shift', buffered: true });
 }
 
+export function longTaskReporter(options) {
+  return (entry) => {
+    log(`${entry.duration}ms`, entry.startTime, 'tbt');
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
 function trackLongTasks(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      log(`${entry.duration}ms`, entry.startTime, 'tbt');
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntries().forEach(longTaskReporter(options));
   });
   observer.observe({ type: 'longtask', buffered: true });
 }
 
+export function firstPaintReporter(options) {
+  return (entry) => {
+    log('', entry.startTime, 'fp');
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
 function trackFirstPaint(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntriesByName('first-paint').forEach((entry) => {
-      log('', entry.startTime, 'fp');
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntriesByName('first-paint').forEach(firstPaintReporter(options));
   });
   observer.observe({ type: 'paint', buffered: true });
 }
 
-function trackResources(options) {
+export function resourceLoadReporter(options) {
+  return (entry) => {
+    const blockingStatus = entry.renderBlockingStatus === 'blocking' ? ` (by ${entry.initiatorType}, ${entry.renderBlockingStatus})` : '';
+    log(`${entry.name}${blockingStatus}`, Math.round(entry.startTime + entry.duration), 'load');
+    if (options.debug) {
+      console.log(JSON.stringify(entry));
+    }
+  };
+}
+
+function trackResourceLoads(options) {
   const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      const blockingStatus = entry.renderBlockingStatus === 'blocking' ? ' (by ' + entry.initiatorType + ', ' + entry.renderBlockingStatus + ')' : '';
-      log(`${entry.name}${blockingStatus}`, Math.round(entry.startTime + entry.duration), 'load');
-      if (options.debug) {
-        console.log(JSON.stringify(entry));
-      }
-    });
+    list.getEntries().forEach(resourceLoadReporter(options));
   });
   observer.observe({ type: 'resource', buffered: true });
 }
 
-export const init = (options) => {
+export function init(options = {}) {
   log('Starting Franklin performance logger');
   const config = { ...DEFAULT_OPTIONS, ...options };
   try {
@@ -189,9 +224,10 @@ export const init = (options) => {
       trackLongTasks(options);
     }
     if (config.resources) {
-      trackResources(options);
+      trackResourceLoads(options);
     }
-  } catch (e) {
-    // no output
+  } catch (err) {
+    // Prevent exception from bubbling up and breaking main script.
+    console.log(err);
   }
 }
